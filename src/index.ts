a9118147aa691830ffada6f5191d64cdb53549c9
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2022-04-18 16:19:07 +0700

    adding score and restart button

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ document.addEventListener('DOMContentLoaded',() => {
     let bird = document.querySelector('.bird') as HTMLInputElement;
     let ground = document.querySelector('.ground') as HTMLInputElement;
     let gameDisplay = document.querySelector('.game-container') as HTMLInputElement;
-
     let position_bird_left = 220;
     let position_bird_bottom = 180;
     let gravity = 2;
     let click_action = false;
+    let gap = 450;
+    let currentScore = 0;
     
     let isGameOver = false;
     var lastTimeCall_generateObstacleFunction: number;
@@ -18,24 +19,41 @@ document.addEventListener('DOMContentLoaded',() => {
         let obstacleLeft = 500;
         let obstacleBottom = randomHeight;
         var obstacle = document.createElement('div');
-        obstacle.classList.add('obstacle');
+        var topObstacle = document.createElement('div');
+        var updateScore = false;
+        if(isGameOver == false) {
+            obstacle.classList.add('obstacle');
+            topObstacle.classList.add('topObstacle');
+        }
         gameDisplay.appendChild(obstacle);
+        gameDisplay.appendChild(topObstacle);
         obstacle.style.left = obstacleLeft + 'px';
         obstacle.style.bottom = obstacleBottom + 'px';
-
+        topObstacle.style.left = obstacleLeft + 'px';
+        topObstacle.style.bottom = obstacleBottom +gap + 'px';
         function moveObstacle() {
             if(isGameOver == false) {
                 obstacleLeft -= 2;
                 obstacle.style.left = obstacleLeft + 'px';
+                topObstacle.style.left = obstacleLeft + 'px';
                 if(obstacleLeft === -60) {
                     clearInterval(timerId);
                     gameDisplay.removeChild(obstacle);
+                    gameDisplay.removeChild(topObstacle);
                 }
                 if(obstacleLeft > 200 && obstacleLeft < 280 && position_bird_left === 220 && 
-                    position_bird_bottom < obstacleBottom + 153 ||
+                    (position_bird_bottom < obstacleBottom + 153 || 
+                        position_bird_bottom > obstacleBottom + gap - 200) ||
                     position_bird_bottom === 0) {
                     gameOver();
+                    
                 }
+                if(updateScore == false)
+                    if(obstacleLeft + 60 <= position_bird_left ) {
+                        currentScore += 1;
+                        console.log("your score:" + currentScore);
+                        updateScore = true;
+                    }
             }
         }
         let timerId = setInterval(moveObstacle,20);
@@ -60,8 +78,8 @@ document.addEventListener('DOMContentLoaded',() => {
     function processInput() {
         if(click_action == true)
         {   
-            if(position_bird_bottom < 500) position_bird_bottom += 70;
-            console.log(position_bird_bottom);
+            if(position_bird_bottom < 500) position_bird_bottom += 50;
+            //console.log(position_bird_bottom);
             click_action = false;
         }
     }
@@ -82,6 +100,11 @@ document.addEventListener('DOMContentLoaded',() => {
         if(isGameOver == false) {
             bird.style.bottom = position_bird_bottom + 'px';
             bird.style.left = position_bird_left + 'px';
+        }else {
+            var text_score =  document.getElementsByClassName("text_score");
+            text_score[0].innerHTML = "Your score is: " + currentScore;
+            var restart_button = document.querySelector('.restart-button') as HTMLInputElement;
+            restart_button.style.zIndex = "+2";
         }
     }
     setup()
