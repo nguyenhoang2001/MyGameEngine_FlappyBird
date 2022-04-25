289fe281235ddcf7e09914d553596a8207565a9c
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2022-04-25 10:13:45 +0700

    Finish splitting game Engine and Flappy Bird Game and rewrite to camelCase style

--- src/gameEngine.ts
+++ src/gameEngine.ts
@@ -21,6 +21,7 @@ export class Scene {
         this.renderer = new Renderer();
         this.changeScene = false;
     }
+    inputProcessing() {}
     startScene() {}
     update(time: number, delta: number) {}
     render() {}
@@ -41,6 +42,11 @@ export class SceneManager {
         this.changeScene();
     }
 
+    inputProcessing() {
+        this.scenes[this.currentIndex].inputProcessing();
+        this.changeScene();
+    }
+
     addScene(scene: Scene) {
         this.scenes.push(scene)
     }
@@ -58,14 +64,12 @@ export class SceneManager {
             } else {
                 this.currentIndex += 1;
             }
-            console.log('the next scene index: ' + this.currentIndex);
             this.startScene();
         }
     }
 
     render() {
         this.scenes[this.currentIndex].render();
-        //this.changeScene();
     }
 }
 
@@ -106,7 +110,6 @@ export class Game {
     }
 
     start() {
-        //this.constructGameEngine();
         this.lastTime = window.performance.now();
         this.scenes.startScene();
         requestAnimationFrame(()=>this.loop());
@@ -115,7 +118,7 @@ export class Game {
     loop() {
         var delta = window.performance.now() - this.lastTime;
         var time = window.performance.now();
-        // this.input.update(time, delta)
+        this.scenes.inputProcessing();
         this.scenes.update(time, delta);
         this.scenes.render();
         this.lastTime = time;
@@ -124,187 +127,5 @@ export class Game {
 
     addScene(scene: Scene) {
         this.scenes.addScene(scene);
-    }
-}
-
-export class StartSceneFlappyBird extends Scene {
-    startButton!: {objectGame:ObjectGame};
-    constructor() {
-        super();
-        this.startButton = {objectGame: new ObjectGame('.game-container','start-button',"BUTTON",175,330,200,50)};
-    }
-
-    initInputEvent() {
-        this.startButton.objectGame.object.onclick = () => { 
-            this.changeScene = true;
-            this.startButton.objectGame.object.remove();
-        }
-    }
-
-    update(time: number, delta: number): void {
-
-    }
-
-    startScene(): void {
-        this.startButton.objectGame.configureObject();
-        this.startButton.objectGame.object.innerText = 'Play';
-        this.initInputEvent();
-    }
-
-    render(): void {
-        this.renderer.renderObject(this.startButton);
-    }
-}
-
-export class EndingSceneFlappyBird extends Scene {
-    textScore!: {objectGame:ObjectGame};
-    textHighScore!: {objectGame:ObjectGame};
-    restartButton!: {objectGame:ObjectGame};
-    score: number;
-    highScore:number;
-    constructor() {
-        super();
-        this.score = 0;
-        this.highScore = 0;
-        this.textScore = {objectGame: new ObjectGame('.game-container', 'text_score','div',180,430,200,45)};
-        this.textHighScore = {objectGame: new ObjectGame('.game-container', 'high_score','div',180,520,200,45)};
-        this.restartButton = {objectGame: new ObjectGame('.game-container', 'restart-button','BUTTON',175,330,200,50)};
-    }
-
-    initInputEvent() {
-        this.restartButton.objectGame.object.onclick = () => {
-            this.changeScene = true;
-            this.textScore.objectGame.object.remove();
-            this.textHighScore.objectGame.object.remove();
-            this.restartButton.objectGame.object.remove();
-        }
-    }
-
-    startScene(): void {
-        this.textScore.objectGame.configureObject();
-        this.textHighScore.objectGame.configureObject();
-        this.restartButton.objectGame.configureObject();
-        this.updateNewScore(passingScore);
-        this.textScore.objectGame.object.innerHTML = 'Your score: ' + this.score.toString();
-        this.textHighScore.objectGame.object.innerHTML = 'High score: ' + this.highScore.toString();
-        this.restartButton.objectGame.object.innerText = 'Play again';
-        this.initInputEvent();
-    }
-
-    updateNewScore(newScore:number) {
-        this.score = newScore;
-        if(this.highScore < newScore) {
-            this.highScore = newScore;
-        }
-    }
-
-    render(): void {
-        this.renderer.renderObject(this.textScore);
-        this.renderer.renderObject(this.textHighScore);
-        this.renderer.renderObject(this.restartButton);
-    }
-}
-
-class Pipe extends ObjectGame {
-    updateCheck!: boolean;
-    constructor(parrentId:string, className:string, objectId:string, left:number, bottom:number, width:number, height:number) {
-        super(parrentId,className,objectId,left,bottom,width,height);
-        this.updateCheck = false;
-    }
-}
-
-export class GamePlaySceneFlappyBird extends Scene {
-    bird!: {objectGame:ObjectGame};
-    textScore!: {objectGame:ObjectGame};
-    pipeDown!: {objectGame:Pipe[]};
-    pipeUp!: {objectGame:Pipe[]};
-    score!: number;
-    isGameOver!:boolean;
-    gravity!:number;
-    gap!:number;
-    lastTimeGeneratePipe!:number;
-    constructor() {
-        super();
-        this.bird = {objectGame: new ObjectGame('.sky','bird','div',220,180,60,45)};
-        this.textScore = {objectGame: new ObjectGame('.game-container','counter_score','div',220,570,100,45)};
-        this.pipeDown = {objectGame: []};
-        this.pipeUp = {objectGame: []};
-        this.score = 0;
-        this.isGameOver = false;
-        this.gravity = 2;
-        this.gap = 450;
-        this.lastTimeGeneratePipe = 0;
-    }
-
-    generatePipe() {
-        let randomHeight = Math.random() * 60;
-        this.pipeDown.objectGame.push(new Pipe('.game-container','obstacle','div',500,randomHeight,60,300));
-        this.pipeUp.objectGame.push(new Pipe('.game-container','topObstacle','div',500,randomHeight + this.gap,60,300));
-        let lengthPipe = this.pipeDown.objectGame.length;
-        this.pipeDown.objectGame[lengthPipe].configureObject();
-        this.pipeUp.objectGame[lengthPipe].configureObject();
-    }
-
-    moveObstacle() {
-        for(let i = 0; i < this.pipeDown.objectGame.length; i++) {
-            this.pipeDown.objectGame[i].left -= 2;
-            this.pipeUp.objectGame[i].left -= 2;
-            if(this.pipeDown.objectGame[i].updateCheck == false) {
-                if(this.pipeDown.objectGame[i].left + 60 <= this.bird.objectGame.left) {
-                    this.score += 1;
-                    console.log('Your score: '+this.score);
-                    this.pipeDown.objectGame[i].updateCheck = true;
-                    this.pipeUp.objectGame[i].updateCheck = true;
-                }
-            }
-        }
-        if(this.pipeDown.objectGame.length > 0 && this.pipeDown.objectGame[0].left == -60) {
-            this.pipeDown.objectGame[0].object.remove();
-            this.pipeUp.objectGame[0].object.remove();
-            this.pipeDown.objectGame.shift();
-            this.pipeUp.objectGame.shift();
-        }
-        /// game over.....
-    }
-
-    initInputEvent() {
-        document.addEventListener('click', () => {
-            if(this.bird.objectGame.bottom < 500)
-                this.bird.objectGame.bottom += 50;
-        });
-    }
-
-    startScene(): void {
-        this.score = 0;
-        this.isGameOver = false;
-        this.bird.objectGame.bottom = 180;
-        this.pipeDown = {objectGame: []};
-        this.pipeUp = {objectGame: []};
-        this.bird.objectGame.configureObject();
-        this.textScore.objectGame.configureObject();
-        this.textScore.objectGame.object.innerHTML = this.score.toString();
-        this.lastTimeGeneratePipe = window.performance.now();
-        this.generatePipe();
-    }
-
-    render(): void {
-        this.renderer.renderObject(this.bird);
-        this.renderer.renderObject(this.textScore);
-        this.renderer.renderManyObjects(this.pipeDown);
-        this.renderer.renderManyObjects(this.pipeUp);
-    }
-}
-
-let passingScore = 0;
-
-export class FlappyBirdGame extends Game {
-    startScene:StartSceneFlappyBird;
-    endingScene:EndingSceneFlappyBird;
-    constructor() {
-        super();
-        this.startScene = new StartSceneFlappyBird();
-        this.endingScene = new EndingSceneFlappyBird();
-        this.scenes.addScene(this.startScene);
-        this.scenes.addScene(this.endingScene);
     }
 }
